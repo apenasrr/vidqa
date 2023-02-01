ec4476f40560ca149f467dee467f27732c98ed16
apenasrr <apenasrr@gmail.com>
2023-02-01 17:15:15 -0300

    v0.1.10

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/apenasrr/vidqa",
-    version="0.1.8",
+    version="0.1.10",
     zip_safe=False,
 )
